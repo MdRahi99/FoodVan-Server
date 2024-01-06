f7f8adefc23f1b9e8f9a78bc0fcf6f9f85582d6d
MD Rahi <fhrstation21@gmail.com>
2024-01-07 01:27:32 +0600

    ...

--- index.js
+++ index.js
@@ -1,22 +1,23 @@
 const express = require('express');
-const axios = require('axios');
 const cors = require('cors');
 const app = express();
+
 const PORT = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
 
-app.get('/api/items', async (req, res) => {
-    try {
-        const response = await axios.get('http://www.api.technicaltest.quadtheoryltd.com/api/Item?page=1&pageSize=10');
-        res.json(response.data.Items);
-    } catch (error) {
-        console.error(error);
-        res.status(500).json({ error: 'Internal Server Error' });
-    }
+const jsonData = require('./Data/items.json');
+
+app.get('/api/items', (req, res) => {
+  try {
+    res.json(jsonData);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
 });
 
 app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
+  console.log(`Server is running on port ${PORT}`);
 });
