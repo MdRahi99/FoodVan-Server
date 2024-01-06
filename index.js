651e5e07c35858a2bc85e24db1303e6acfdd2bae
MD Rahi <fhrstation21@gmail.com>
2024-01-07 00:19:50 +0600

    ...

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const axios = require('axios');
+const cors = require('cors');
 const app = express();
 const PORT = process.env.PORT || 5000;
 
+app.use(cors());
 app.use(express.json());
 
 app.get('/api/items', async (req, res) => {
